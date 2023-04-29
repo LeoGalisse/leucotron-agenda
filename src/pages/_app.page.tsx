6869bc13720f11cc465d00e6cdc63602ad5dc618
LeoGalisse <leonardogalisse@gmail.com>
2023-04-29 16:23:18 -0300

    feat: add connect calendar page

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,25 @@
 import { globalStyles } from '@component/styles/global'
+import { SessionProvider } from 'next-auth/react'
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          locale: 'pt_BR',
+          url: '',
+          siteName: 'Leucotron Agenda',
+        }}
+      />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
