420801dca039a6b7057cd66209dae987b6ab024d
LeoGalisse <leonardogalisse@gmail.com>
2023-04-30 02:10:45 -0300

    style: fix style position for calendar

--- src/pages/schedule/[username]/styles.ts
+++ src/pages/schedule/[username]/styles.ts
@@ -3,13 +3,19 @@ import { Heading, styled } from '@leucotron-ui/react'
 export const Container = styled('div', {
   maxWidth: 852,
   padding: '0 $4',
-  margin: '$20 auto $4',
+  margin: 'auto',
 })
 
 export const UserHeader = styled('div', {
   display: 'flex',
   alignItems: 'center',
   flexDirection: 'column',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+
+  paddingLeft: '$4',
+  paddingTop: '$4',
 
   [`> ${Heading}`]: {
     lineHeight: '$base',
