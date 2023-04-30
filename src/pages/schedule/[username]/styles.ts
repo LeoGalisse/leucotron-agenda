7c269e038da28e4a1447c3816f7057e913f491dd
LeoGalisse <leonardogalisse@gmail.com>
2023-04-30 00:59:53 -0300

    feat: add schedule page body

---
+++ src/pages/schedule/[username]/styles.ts
@@ -0,0 +1,18 @@
+import { Heading, styled } from '@leucotron-ui/react'
+
+export const Container = styled('div', {
+  maxWidth: 852,
+  padding: '0 $4',
+  margin: '$20 auto $4',
+})
+
+export const UserHeader = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+
+  [`> ${Heading}`]: {
+    lineHeight: '$base',
+    marginTop: '$2',
+  },
+})
